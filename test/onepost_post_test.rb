4344dc317a8f978832b45ed85d5196058a062232
Adam Darrah <piremies@gmail.com>
2021-02-18 15:44:40 -0500

    Can fetch a single post

--- test/onepost_post_test.rb
+++ test/onepost_post_test.rb
@@ -9,7 +9,7 @@ class OnepostPostTest < Minitest::Test
   end
 
   def test_can_fetch_all_posts
-    stub_request(:get, "https://onepost1.p.rapidapi.com/api/v1/authorized_pages?secret_key=67890")
+    stub_request(:get, "https://onepost1.p.rapidapi.com/api/v1/posts?secret_key=67890")
       .with(
         body: "{}",
         headers: {
@@ -22,8 +22,26 @@ class OnepostPostTest < Minitest::Test
         }
       ).to_return(status: 200, body: example_posts_data.to_json, headers: {})
 
-    data = @client.get_authorized_pages
+    data = @client.get_posts
     assert_equal 3, data["collection"].count
+  end
+
+  def test_can_fetch_one_post
+    stub_request(:get, "https://onepost1.p.rapidapi.com/api/v1/posts/1?secret_key=67890")
+      .with(
+        body: "{}",
+        headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type'=>'application/json',
+          'User-Agent'=>'Ruby',
+          'X-Rapidapi-Host'=>'onepost1.p.rapidapi.com',
+          'X-Rapidapi-Key'=>'12345'
+        }
+      ).to_return(status: 200, body: example_post_data.to_json, headers: {})
+
+    data = @client.get_post(1)
+    assert_equal 1, data["id"]
   end
 
   private
@@ -77,6 +95,15 @@ class OnepostPostTest < Minitest::Test
 
   def example_post_data
     {
+      "id"=>1,
+      "body"=>"Testing from Production",
+      "created_at"=>"2021-02-12T12:02:45.182-05:00",
+      "updated_at"=>"2021-02-12T12:02:55.467-05:00",
+      "state"=>"published",
+      "publish_at"=>nil,
+      "authorized_page_ids"=>[],
+      "image_url"=>nil,
+      "social_posts"=>[]
     }
   end
 end
