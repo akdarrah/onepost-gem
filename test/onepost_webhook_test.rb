7fc4f7c6c17ba6858e019b2a085bf70705f12721
Adam Darrah <piremies@gmail.com>
2021-02-18 17:43:06 -0500

    Can update an existing webhook

--- test/onepost_webhook_test.rb
+++ test/onepost_webhook_test.rb
@@ -71,6 +71,29 @@ class OnepostWebhookTest < Minitest::Test
     assert_equal 2, data["id"]
   end
 
+  def test_can_update_a_webhook
+    stub_request(:put, "https://onepost1.p.rapidapi.com/api/v1/webhooks/2?secret_key=67890")
+      .with(
+        body: "{\"webhook\":{\"endpoint_url\":\"https://webhook.site/3f338283-10b0-492a\"}}",
+        headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'Content-Type'=>'application/json',
+          'User-Agent'=>'Ruby',
+          'X-Rapidapi-Host'=>'onepost1.p.rapidapi.com',
+          'X-Rapidapi-Key'=>'12345'
+        }
+      )
+      .to_return(status: 200, body: example_webhook_data.to_json, headers: {})
+
+    data = @client.update_webhook(2, body: {
+      "webhook": {
+        "endpoint_url": "https://webhook.site/3f338283-10b0-492a"
+      }
+    })
+    assert_equal 2, data["id"]
+  end
+
   private
 
   def example_webhooks_data
